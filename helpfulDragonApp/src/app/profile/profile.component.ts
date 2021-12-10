79ae81613bb067761759cdbcebebfa684de988de
JoshParada <54327710+JoshParada@users.noreply.github.com>
2021-12-10 12:16:27 -0600

    added interface

--- helpfulDragonApp/src/app/profile/profile.component.ts
+++ helpfulDragonApp/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { profile } from './profile';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+
+  dragonsArr:profile[]=[
+    {name:'Dragon (Shrek)',picURL:'',description:'loves donkeys',rates:[1,2,3]},
+    {name:'Dojo',picURL:'',description:'Xiaolin Showdown',rates:[1,2,3]},
+    {name:'Toothless',picURL:'',description:'great dental hygiene',rates:[1,2,3]},
+    {name:'Falkor',picURL:'',description:'very fluffy',rates:[1,2,3]},
+    {name:'Mushu',picURL:'',description:'charming',rates:[1,2,3]},
+    {name:'Shenron',picURL:'',description:'powerful',rates:[1,2,3]},
+    {name:'Charizard',picURL:'',description:'hot af',rates:[1,2,3]}
+  ]
 
   constructor() { }
 
---
+++ helpfulDragonApp/src/app/profile/profile.ts
@@ -0,0 +1,6 @@
+export interface profile{
+    name:string,
+    picURL:string,
+    description:string,
+    rates:[number,number,number]
+}
