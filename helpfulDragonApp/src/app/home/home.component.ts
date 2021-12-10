0b915d3b4fa3aea530d0779573a1b76097dc4811
PatoGMTP <93350228+PatoGMTP@users.noreply.github.com>
2021-12-10 14:33:46 -0600

    More layout stuff

--- helpfulDragonApp/src/app/home/home.component.ts
+++ helpfulDragonApp/src/app/home/home.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  review: {title: string, text: string} = {title: "", text: ""};
+
+  index: number = 0;
+
+  review_list: {title: string, text: string}[] = [
+    {title: "1", text: "11"},
+    {title: "2", text: "22"},
+    {title: "3", text: "33"},
+    {title: "4", text: "44"},
+    {title: "5", text: "55"},
+  ];
+
+  constructor()
+  {
+    this.review = this.review_list[this.index];
+  }
 
   ngOnInit(): void {
   }
 
+  changeSlide(input: number): void
+  {
+    this.index = (this.index + input) % this.review_list.length;
+    if (this.index === -1) this.index = this.review_list.length-1;
+    this.review = this.review_list[this.index];
+  }
 }
